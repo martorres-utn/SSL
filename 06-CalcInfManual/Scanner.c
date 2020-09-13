a7eb70380388213658c5ac55a7edb30265696630
Marcos Torres <martorres@est.frba.utn.edu.ar>
2020-09-12 23:01:18 -0300

    Reemp: Trans codificadas por punteros a funciones

--- 06-CalcInfManual/Scanner.c
+++ 06-CalcInfManual/Scanner.c
@@ -1,128 +1,65 @@
 #include <stdio.h>
 #include "Scanner.h"
 
+Token FoundLexicalError(int symbol)
+{
+    printf("[!LEXICAL_ERROR!]\n");
+    return T_END;
+}
+
+Token FoundID(int symbol)
+{
+    return T_ID;
+}
+
+Token FoundConstant(int symbol)
+{
+    return T_CONSTANT;
+}
+
 Token Scanner_GetNextToken()
 {
     int newChar = EOF;
     State nextState = S_EXPR;
     Token foundToken = T_INITIAL;
     
+    // check if there is a remaining token from a previous call like operators: +, *
     if(RemainingToken != T_INITIAL)
     {
         foundToken = RemainingToken;
         RemainingToken = T_INITIAL;
-    }
-
-    if(foundToken != T_INITIAL)
         return foundToken;
-
+    }
+    
+    //get char from input stream
     while((newChar = getchar()) != EOF)
     {
-        foundToken = T_INITIAL;
+        foundToken = T_INITIAL; //will containt a token if we find one
 
         switch(newChar)
         {
+            //[a-z]
             case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
             {
                 nextState = S_ID;
-                
-                switch(LastState)
-                {
-                    case S_EXPR:
-                    {
-                        break;
-                    }
-                    case S_ID:
-                    {
-                        break;
-                    }
-                    case S_CONSTANT:
-                    {
-                        printf("[!LEXICAL_ERROR!]\n");
-                        foundToken = T_END;
-                        break;
-                    }
-                }
                 break;
             }
+            //[0-9]
             case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
             {
                 nextState = S_CONSTANT;
-
-                switch(LastState)
-                {
-                    case S_EXPR:
-                    {
-                        break;
-                    }
-                    case S_ID:
-                    {
-                        printf("[!LEXICAL_ERROR!]\n");
-                        foundToken = T_END;
-                        break;
-                    }
-                    case S_CONSTANT:
-                    {
-                        break;
-                    }
-                }
-
                 break;
             }
             case '+':
             {
                 nextState = S_EXPR;
-
-                switch(LastState)
-                {
-                    case S_EXPR:
-                    {
-                        printf("[!LEXICAL_ERROR!]\n");
-                        foundToken = T_END;
-                        break;
-                    }
-                    case S_ID:
-                    {
-                        foundToken = T_ID;
-                        break;
-                    }
-                    case S_CONSTANT:
-                    {
-                        foundToken = T_CONSTANT;
-                        break;
-                    }
-                }
-                
                 RemainingToken = T_OP_PLUS;
-
                 break;
             }
             case '*':
             {
                 nextState = S_EXPR;
-
-                switch(LastState)
-                {
-                    case S_EXPR:
-                    {
-                        printf("[!LEXICAL_ERROR!]\n");
-                        foundToken = T_END;
-                        break;
-                    }
-                    case S_ID:
-                    {
-                        foundToken = T_ID;
-                        break;
-                    }
-                    case S_CONSTANT:
-                    {
-                        foundToken = T_CONSTANT;
-                        break;
-                    }
-                }
-                
                 RemainingToken = T_OP_PROD;
-                
                 break;
             }
             default:
@@ -132,11 +69,20 @@ Token Scanner_GetNextToken()
             }
         }
         
+        //Check if there is a transition defined for: LastState -> nextState
+        if(TokenReturningMatrix[LastState][nextState] != NULL)
+            foundToken = TokenReturningMatrix[LastState][nextState](newChar); //get the token from the transition
+
         LastState = nextState;
 
+        //return a new token only if we find one
         if(foundToken != T_INITIAL)
             return foundToken;
     }
+
+    //getchar() == EOF
+    
+    //once we reach the end we need to return the corresponding token according to the previous state
 
     switch(LastState)
     {
@@ -148,16 +94,16 @@ Token Scanner_GetNextToken()
         case S_ID:
         {
             foundToken = T_ID;
+            RemainingToken = T_END;
             break;
         }
         case S_CONSTANT:
         {
             foundToken = T_CONSTANT;
+            RemainingToken = T_END;
             break;
         }
     }
-
-    RemainingToken = T_END;
 
     return foundToken;
 }
--- 06-CalcInfManual/Scanner.h
+++ 06-CalcInfManual/Scanner.h
@@ -17,8 +17,32 @@ enum PossibleStates {
 
 typedef enum PossibleTokens Token;
 typedef enum PossibleStates State;
+typedef Token (*TokenReturningTransition)(int);
 
 static State LastState = S_EXPR;
 static Token RemainingToken = T_INITIAL;
+
+
+Token FoundLexicalError();
+Token FoundID();
+Token FoundConstant();
+
+static TokenReturningTransition TokenReturningMatrix[3][3] = {
+    {
+        &FoundLexicalError, //S_EXPR -> S_EXPR
+        NULL,               //S_EXPR -> S_ID
+        NULL                //S_EXPR -> S_CONSTANT
+    },
+    {
+        &FoundID,           //S_ID -> S_EXPR
+        NULL,               //S_ID -> S_ID
+        &FoundLexicalError  //S_ID -> S_CONSTANT
+    },
+    {
+        &FoundConstant,         //S_CONSTANT -> S_EXPR
+        &FoundLexicalError,     //S_CONSTANT -> S_ID
+        NULL                    //S_CONSTANT -> S_CONSTANT
+    }
+};
 
 Token Scanner_GetNextToken();
