c9b91b557b1115492d4fb324b79a4ff0075575e7
Marcos Torres <martorres@est.frba.utn.edu.ar>
2020-09-20 20:46:54 -0300

    limpio comentarios

--- 06-CalcInfManual/Scanner2.c
+++ 06-CalcInfManual/Scanner2.c
@@ -2,57 +2,17 @@
 #include <ctype.h>
 #include "Scanner2.h"
 
-/*Token FoundLexicalError(int symbol)
-{
-    printf("[lexical error!]\n");
-    LexicalError = true;
-    return T_END;
-}
-
-Token FoundID(int symbol)
-{
-    ungetc(symbol, stdin);
-    return T_ID;
-}
-
-Token FoundConstant(int symbol)
-{
-    ungetc(symbol, stdin);
-    return T_CONSTANT;
-}*/
-
-//S_EXPR -> S_EXPR Do I wan't to return ++a or +a or + as Lexical Errors? I can detect those kind of strings at this level
-/*Token FoundOperatorOrLexicalError(int symbol)
-{
-    if(LastToken == T_CONSTANT || LastToken == T_ID || LastToken == T_INITIAL)
-    {
-        if(symbol == '+')
-            return T_OP_PLUS;
-        else if(symbol == '*')
-            return T_OP_PROD;
-        else
-            return FoundLexicalError(symbol); //this shouldn't be possible
-    }
-    return FoundLexicalError(symbol);
-}*/
-
 Token Scanner_GetNextToken()
 {
     int newChar = EOF;
-    //State nextState = S_EXPR; //[OLD]
-    //Token foundToken = T_INITIAL; //[OLD]
-    
+
     //get char from input stream
     while((newChar = getchar()) != EOF && newChar != '\n')
     {
-        //foundToken = T_INITIAL; //will containt a token if we find one //[OLD]
-
         if(!isspace(newChar))
         {
             if(isalpha(newChar))
             {
-                //nextState = S_ID; //[OLD]
-
                 //token id [a-zA-Z]*
 				int accepted = 0;
 				while( (newChar = getchar()) != EOF && (accepted = isalpha(newChar)) ) {}
@@ -62,8 +22,6 @@ Token Scanner_GetNextToken()
             }
             else if(isdigit(newChar))
             {
-                //nextState = S_CONSTANT; //[OLD]
-
                 //token const [0-9]*
 				int accepted = 0;
 				while( (newChar = getchar()) != EOF && (accepted = isdigit(newChar)) ) {}
@@ -73,12 +31,10 @@ Token Scanner_GetNextToken()
             }
             else if(newChar == '+')
             {
-                //nextState = S_EXPR; [OLD]
                 return T_OP_PLUS;
             }
             else if(newChar == '*')
             {
-                //nextState = S_EXPR; //[OLD]
                 return T_OP_PROD;
             }
             else if(newChar == '(')
@@ -91,63 +47,14 @@ Token Scanner_GetNextToken()
             }
             else
             {
-                //nextState = S_EXPR; //[OLD]
-                //foundToken = FoundLexicalError(newChar); [OLD]
                 printf("[lexical error!]\n");
                 LexicalError = true;
                 return T_END;
             }
-
-            /*if(!LexicalError)
-            {
-                //Check if there is a transition defined for: LastState -> nextState
-                if(TokenReturningMatrix[LastState][nextState] != NULL)
-                    foundToken = TokenReturningMatrix[LastState][nextState](newChar); //get the token from the transition
-            }*/ //[OLD]
-
-            //LastState = nextState; //[OLD]
-
-            //return a new token only if we find one
-            /*if(foundToken != T_INITIAL)
-            {
-                LastToken = foundToken;
-                return foundToken;
-            }*///[OLD]
         }
     }
-
     //getchar() == EOF or \n
     ReachedEOF = newChar == EOF;
-
-    //once we reach the end we need to return the corresponding token according to the previous state
-
-    /*switch(LastState)
-    {
-        case S_EXPR:
-        {
-            foundToken = T_END;
-            break;
-        }
-        case S_ID:
-        {
-            foundToken = T_ID;
-            if(newChar == '\n')
-                ungetc(newChar, stdin);
-            break;
-        }
-        case S_CONSTANT:
-        {
-            foundToken = T_CONSTANT;
-            if(newChar == '\n')
-                ungetc(newChar, stdin);
-            break;
-        }
-    }*/ //[OLD]
-    
-    //LastState = S_EXPR; //return to initial state //[OLD]
-    //LastToken = foundToken;//[OLD]
-    //return foundToken;//[OLD]
-
     return T_END;
 }
 
@@ -174,6 +81,4 @@ void Scanner_MoveToNextExpression()
 void Scanner_ResetInternalState()
 {
     LexicalError = false; //clean flag
-    //LastToken = T_INITIAL;
-    //LastState = S_EXPR;
 }
--- 06-CalcInfManual/Scanner2.h
+++ 06-CalcInfManual/Scanner2.h
@@ -16,43 +16,10 @@ enum PossibleTokens {
     T_END
 };
 
-/*enum PossibleStates {
-    S_EXPR = 0,
-    S_ID,
-    S_CONSTANT
-};*/
-
 typedef enum PossibleTokens Token;
-//typedef enum PossibleStates State;
-//typedef Token (*TokenReturningTransition)(int);
 
-//static State LastState = S_EXPR;
-//static Token LastToken = T_INITIAL;
 static bool ReachedEOF = false;
 static bool LexicalError = false;
-
-/*Token FoundLexicalError();
-Token FoundID();
-Token FoundConstant();
-Token FoundOperatorOrLexicalError();*/
-
-/*static TokenReturningTransition TokenReturningMatrix[3][3] = {
-    {
-        &FoundOperatorOrLexicalError, //S_EXPR -> S_EXPR
-        NULL,               //S_EXPR -> S_ID
-        NULL                //S_EXPR -> S_CONSTANT
-    },
-    {
-        &FoundID,           //S_ID -> S_EXPR
-        NULL,               //S_ID -> S_ID
-        &FoundLexicalError  //S_ID -> S_CONSTANT
-    },
-    {
-        &FoundConstant,         //S_CONSTANT -> S_EXPR
-        &FoundLexicalError,     //S_CONSTANT -> S_ID
-        NULL                    //S_CONSTANT -> S_CONSTANT
-    }
-};*/
 
 Token Scanner_GetNextToken();
 bool Scanner_HasReachedEOF();
